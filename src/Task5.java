0e6648159be22df88558ded4979e36c77317e33b
Mahin <mahin610808@gmail.com>
2026-04-22 21:25:26 +0600

    Local

---
+++ src/Task5.java
@@ -0,0 +1,42 @@
+abstract  class Loan{
+    int loanAmount=100000;
+    abstract void calculateInterest();
+}
+class HomeLoan extends Loan{
+
+    double interestRate=0.08;
+    public void calculateInterest(){
+        double interest=(loanAmount*interestRate);
+        System.out.println("HomeLoan Interest = "+ (int)interest+" "+ "BDT");
+    }
+}
+class CarLoan extends Loan{
+    int loanAmount=100000;
+    double interestRate=0.1;
+    public void calculateInterest(){
+        double interest=(loanAmount*interestRate);
+        System.out.println("CarLoan Interest = "+ (int)interest+" "+"BDT");
+    }
+}
+class EducationLoan extends Loan{
+    int loanAmount=100000;
+    double interestRate=0.06;
+    public void calculateInterest(){
+        double interest=(loanAmount*interestRate);
+        System.out.println("EducationLoan Interest = "+ (int)interest+" "+"BDT");
+    }
+}
+
+public class Task5 {
+    public static void main(String[] args) {
+        Loan[] l={
+                new HomeLoan(),
+                new CarLoan(),
+                new EducationLoan()
+        };
+        for(Loan x:l){
+            x.calculateInterest();
+            System.out.println();
+        }
+    }
+}
